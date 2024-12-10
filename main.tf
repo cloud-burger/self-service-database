79a89741fe2150ace2d1662e9a2bec8f0e70b866
Daniel Aguiar <daniel_aguiar_alves@hotmail.com>
2024-12-09 21:14:49 -0300

    feat: implements database infrastructure

---
+++ main.tf
@@ -0,0 +1,16 @@
+module "global_variables" {
+  source      = "./modules/global_variables"
+  environment = var.environment
+}
+
+provider "aws" {
+  region = module.global_variables.aws_region
+}
+
+terraform {
+  backend "s3" {
+    bucket = "cloud-burger-state"
+    key    = "prod/database.tfstate"
+    region = "us-east-2"
+  }
+}

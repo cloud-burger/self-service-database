deff20113e916ff3a0ae6c417fc9f7ad0f0f573f
Daniel Alves <daniel_aguiar_alves@hotmail.com>
2024-12-17 01:44:19 -0300

    chore: add eks state (#13)

--- aws_db_subnet_group.tf
+++ aws_db_subnet_group.tf
@@ -1,4 +1,4 @@
 resource "aws_db_subnet_group" "main" {
   name       = "main"
-  subnet_ids = [aws_subnet.public_subnet.id, aws_subnet.private_subnet.id]
+  subnet_ids = concat(local.aws_public_subnets, local.aws_private_subnets)
 }
--- main.tf
+++ main.tf
@@ -7,6 +7,22 @@ provider "aws" {
   region = module.global_variables.aws_region
 }
 
+data "terraform_remote_state" "eks_state" {
+  backend = "s3"
+
+  config = {
+    bucket = "cloud-burger-states"
+    key    = "prod/eks.tfstate"
+    region = "us-east-1"
+  }
+}
+
+locals {
+  aws_vpc_id          = data.terraform_remote_state.eks_state.outputs.vpc_id
+  aws_public_subnets  = data.terraform_remote_state.eks_state.outputs.public_subnets
+  aws_private_subnets = data.terraform_remote_state.eks_state.outputs.private_subnets
+}
+
 terraform {
   backend "s3" {
     bucket = "cloud-burger-states"

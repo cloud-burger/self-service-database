173f3a36b4011e50d6222aec18a577c173ec2dcb
Daniel Aguiar <daniel_aguiar_alves@hotmail.com>
2024-12-17 01:35:52 -0300

    chore: add eks state

--- aws_security_group.tf
+++ aws_security_group.tf
@@ -1,29 +1,7 @@
-resource "aws_security_group" "private_sg" {
-  name        = "private_sg"
-  description = "Allow all inbound and outbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  # Permitir todo o tráfego de entrada TCP
-  ingress {
-    from_port   = 0
-    to_port     = 65535 # Portas TCP variam de 0 a 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Permitir tráfego de saída para a Internet
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 resource "aws_security_group" "rds_public_sg" {
   name        = "rds_public_sg"
   description = "Allow postgres inbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = local.aws_vpc_id
 
   # Permitir tráfego de entrada apenas nas portas necessárias
   ingress {
@@ -31,24 +9,5 @@ resource "aws_security_group" "rds_public_sg" {
     to_port     = 5432
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "ssm_endpoint_sg" {
-  name   = "ssm_endpoint_sg"
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] # Permite tráfego de entrada de qualquer lugar
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"] # Permite todo o tráfego de saída para qualquer lugar
   }
 }
